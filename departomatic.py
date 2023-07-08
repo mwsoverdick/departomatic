7c25bd757e90f88309f19a7572de3037856a1d49
Mitchell Overdick <mwsoverdick@users.noreply.github.com>
2023-07-08 11:47:50 -0700

    1 pylint fails in initial commit (#2)
    * Address some pylint errors
    
    * add requirements.txt
    
    * Add requirements.txt install to pylint.yml
    
    * Remove broad exception catching
    
    * Convert to a class
    
    * Fix class pylint errors
    
    * Add pylintrc

--- departomatic.py
+++ departomatic.py
@@ -1,11 +1,16 @@
-import pystray
-from PIL import Image, ImageDraw
+"""departomatic.py
+
+Creates a Windows system tray icon to let you know when to leave for the bus
+"""
+
 import time
 import threading
-from datetime import datetime, timedelta
 import sys
 import subprocess
-import traceback
+from datetime import datetime, timedelta
+
+import pystray
+from PIL import Image, ImageDraw
 
 from times import read_csv
 from options import get_options
@@ -18,11 +23,20 @@ colors = {
 }
 
 # Initial color of the icon
-color = colors['idk']
-title = "idk"
+COLOR = colors['idk']
+TITLE = "idk"
 
 
-def time_until_next(times, now):
+def time_until_next(departures, now):
+    """Get time until next bus departure
+
+    Args:
+        departures (List[datetime.time]): List of departure times
+        now (datetime): datetime.now()
+
+    Returns:
+        integer: Minutes until next departure
+    """
     # Convert 'now' to a time object if it's a datetime
     if isinstance(now, datetime):
         now = now.time()
@@ -30,11 +44,11 @@ def time_until_next(times, now):
     # Initialize min_diff to a large value
     min_diff = timedelta(days=1)
 
-    for time in times:
+    for departure in departures:
         # Calculate the difference between the time and now
-        if time > now:
+        if departure > now:
             diff = datetime.combine(
-                datetime.today(), time) - datetime.combine(datetime.today(), now)
+                datetime.today(), departure) - datetime.combine(datetime.today(), now)
             # Update min_diff if this difference is the smallest non-negative difference so far
             if diff < min_diff:
                 min_diff = diff
@@ -42,68 +56,77 @@ def time_until_next(times, now):
     return min_diff.total_seconds() / 60  # Return the difference in minutes
 
 
-def create_image():
-    """Creates a new image with the current color"""
-    # Generate an image and a draw instance
-    image = Image.new('RGB', (64, 64), color)
-    dc = ImageDraw.Draw(image)
-    return image
+class Departomatic():
+    """
+    System try icon application showing when to leave for the bus
+    """
 
+    def __init__(self, options, times) -> None:
+        self.color = colors['idk']
+        self.options = get_options(options)
+        self.departures = read_csv(times)
 
-def setup(icon):
-    """Sets the icon to visible"""
-    icon.visible = True
-    update_icon(icon)
+        # Create a new system tray icon
+        self.icon = pystray.Icon(
+            "test_icon", self.create_image(), "Departomatic")
+        # Start a new thread that updates the icon
+        threading.Thread(target=self.update_icon, args=(self.icon,)).start()
 
+        self.icon.menu = pystray.Menu(pystray.MenuItem(
+            self.options['route'], lambda: donothing))
 
-def update_icon(icon):
-    """Updates the icon color based on the current time"""
-    global color
+    def run(self):
+        """
+        Start the system tray icon
+        """
+        # Run the system tray icon
+        self.icon.run(self.setup)
 
-    options = get_options('./options.yaml')
+    def create_image(self):
+        """Creates a new image with the current color"""
+        # Generate an image and a draw instance
+        image = Image.new('RGB', (64, 64), self.color)
+        ImageDraw.Draw(image)
+        return image
 
-    while True:
-        now = datetime.now()
-        departures = read_csv('times.csv')
-        time_left = time_until_next(departures, now)
+    def setup(self, icon):
+        """Sets the icon to visible"""
+        icon.visible = True
+        self.update_icon(icon)
 
-        if time_left > options['wait'] or time_left < options['iffy']:
-            color = colors['wait']
-        elif time_left > options['go']:
-            color = colors['go']
-        elif time_left > options['iffy']:
-            color = colors['iffy']
-        else:
-            color = colors['idk']
+    def update_icon(self, icon):
+        """Updates the icon color based on the current time"""
 
-        icon.icon = create_image()
-        icon.title = f"{options['route']}\nDeparts in {time_left:.1f} min"
+        while True:
+            now = datetime.now()
+            time_left = time_until_next(self.departures, now)
 
-        # Wait for a while before the next update
-        time.sleep(15)
+            if time_left > self.options['wait'] or time_left < self.options['iffy']:
+                self.color = colors['wait']
+            elif time_left > self.options['go']:
+                self.color = colors['go']
+            elif time_left > self.options['iffy']:
+                self.color = colors['iffy']
+            else:
+                self.color = colors['idk']
+
+            icon.icon = self.create_image()
+            icon.title = f"{self.options['route']}\nDeparts in {time_left:.1f} min"
+
+            # Wait for a while before the next update
+            time.sleep(15)
 
 
 def donothing():
-    pass
+    """Does nothing
+    """
 
 
 def main():
-    while True:
-        try:
-            # Create a new system tray icon
-            icon = pystray.Icon(
-                "test_icon", create_image(), "Departomatic")
-            # Start a new thread that updates the icon
-            threading.Thread(target=update_icon, args=(icon,)).start()
-
-            options = get_options('./options.yaml')
-            icon.menu = pystray.Menu(pystray.MenuItem(
-                options['route'], lambda: donothing))
-
-            # Run the system tray icon
-            icon.run(setup)
-        except Exception as e:
-            print(traceback.extract_tb(e))
+    """Main program loop, runs indefinitely
+    """
+    departo = Departomatic('./options.yaml', './times.csv')
+    departo.run()
 
 
 if __name__ == '__main__':
@@ -111,7 +134,8 @@ if __name__ == '__main__':
         main()
     else:
         # Use pythonw to start this script as a background process
-        # Pass --background argument to indicate that the script is now running as a background process
+        # Pass --background argument to indicate that the script is
+        # now running as a background process
         subprocess.Popen(['pythonw', __file__, '--background'])
 
         # Exit the parent process
